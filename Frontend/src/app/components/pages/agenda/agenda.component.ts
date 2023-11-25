c196f1ed1fc76c95e57d1c7a2c464759eee93be3
jpbr66 <jburgos1000@gmail.com>
2023-11-25 01:56:10 -0600

    update calendar

--- Frontend/src/app/components/pages/agenda/agenda.component.ts
+++ Frontend/src/app/components/pages/agenda/agenda.component.ts
@@ -4,51 +4,6 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin, { Draggable } from '@fullcalendar/interaction';
 
-document.addEventListener('page', function() {
-
-  var containerEl = document.getElementById('external-events');
-  var calendarEl = document.getElementById('calendar');
-  var checkbox = document.getElementById('drop-remove') as HTMLInputElement;
-
-  // initialize the external events
-  // -----------------------------------------------------------------
-  if (containerEl != null) {
-    new Draggable(containerEl, {
-      itemSelector: '.fc-event',
-      eventData: function(eventEl) {
-        return {
-          title: eventEl.innerText
-        };
-      }
-    });
-  }
-
-  // initialize the calendar
-  // -----------------------------------------------------------------
-  if (calendarEl != null) {
-    var calendar = new Calendar(calendarEl, {
-      headerToolbar: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'dayGridMonth,timeGridWeek,timeGridDay'
-      },
-      editable: true,
-      droppable: true, // this allows things to be dropped onto the calendar
-      drop: function(info) {
-        // is the "remove after drop" checkbox checked?
-        if (checkbox != null && checkbox.checked && info.draggedEl.parentNode != null) {
-          // if so, remove the element from the "Draggable Events" list
-          info.draggedEl.parentNode.removeChild(info.draggedEl);
-        }
-      }
-    });
-    calendar.render();
-  }
-
-  
-});
-
-
 @Component({
   selector: 'app-agenda',
   templateUrl: './agenda.component.html',
@@ -56,44 +11,76 @@ document.addEventListener('page', function() {
 })
 export class AgendaComponent {
   
+  constructor(){}
+
   calendarOptions: CalendarOptions = {
     headerToolbar: {
       left: 'prev,next,today',
       center: 'title',
       right: 'dayGridMonth,timeGridWeek,timeGridDay'
     },
+    initialView: 'timeGridWeek',
     editable: true,
-    drop: function(info){
-      var checkbox = document.getElementById('drop-remove') as HTMLInputElement;
-      if (checkbox && checkbox.checked && info.draggedEl.parentNode) {
-        // if so, remove the element from the "Draggable Events" list
-        info.draggedEl.parentNode.removeChild(info.draggedEl);
-      }
-    },
-    //initialView: 'dayGridMonth',
+    navLinks:true,
     plugins: [timeGridPlugin, dayGridPlugin, interactionPlugin]
   };
 
-  Events = []
-
-
-  onDateClick(res:{dateStr:string}) {
-    alert('Date clicked '+ res.dateStr)
+  eventData(eventEl: HTMLElement){
+    var singleslot = (document.getElementById("singleslot") as HTMLDivElement);
   }
 
-  unSelect(){
-    const buttonAdd = document.getElementsByName('buttonAgregar');
-    buttonAdd.forEach(function(button){
-      (button as HTMLButtonElement).disabled = false;
-    })
+  ngOnInit(){
+    var containerEl = document.getElementById('external-events');
+    if (containerEl){
+      new Draggable(containerEl, {
+        itemSelector: '.fc-event',
+        eventData: function(eventEl) {
+          return {
+            title: eventEl.innerText,
+          };
+        }
+      });
+    }
   }
+}
 
-  onUnSelect() {
-    const buttonAdd = (document.getElementsByName('buttonAgregar'));
-    buttonAdd.forEach(function(button){
-      (button as HTMLButtonElement).disabled = true;
-    })
-  }
+/*
+export class AgendaComponent {
+  
+  eventosList:string[] = ['My Event 1', 'My Event 2', 'My Event 3', 'My Event 4', 'My Event 5'];
+
+  calendarOptions: CalendarOptions = {
+    headerToolbar: {
+      left: 'prev,next,today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay'
+    },
+    initialView: 'dayGridMonth',
+    editable: true,
+    navLinks:true,
+    plugins: [timeGridPlugin, dayGridPlugin, interactionPlugin]
+  };
+
+  // Events = []
+
+
+  // onDateClick(res:{dateStr:string}) {
+  //   alert('Date clicked '+ res.dateStr)
+  // }
+
+  // unSelect(){
+  //   const buttonAdd = document.getElementsByName('buttonAgregar');
+  //   buttonAdd.forEach(function(button){
+  //     (button as HTMLButtonElement).disabled = false;
+  //   })
+  // }
+
+  // onUnSelect() {
+  //   const buttonAdd = (document.getElementsByName('buttonAgregar'));
+  //   buttonAdd.forEach(function(button){
+  //     (button as HTMLButtonElement).disabled = true;
+  //   })
+  // }
 
   constructor(){}
 
@@ -102,26 +89,30 @@ export class AgendaComponent {
     // buttonAdd.forEach(function(button){
     //   (button as HTMLButtonElement).disabled = true;
     // })
-    setTimeout(()=>{
-      this.calendarOptions = {
-        initialView: '',
-        selectable: true,
-        select:this.unSelect.bind(this),
-        unselect: this.onUnSelect.bind(this),
-        dateClick: this.onDateClick.bind(this),
-        events: this.Events
-      }
-    })
+    // setTimeout(()=>{
+    //   this.calendarOptions = {
+    //     initialView: '',
+    //     selectable: true,
+    //     select:this.unSelect.bind(this),
+    //     unselect: this.onUnSelect.bind(this),
+    //     dateClick: this.onDateClick.bind(this),
+    //     events: this.Events
+    //   }
+    // })
 
     var containerEl = document.getElementById('external-events');
-    if (containerEl)
+    if (containerEl){
       new Draggable(containerEl, {
         itemSelector: '.fc-event',
         eventData: function(eventEl) {
+          alert(eventEl.innerText + "       " + containerEl?.innerText)
           return {
             title: eventEl.innerText
           };
         }
       });
+      alert(x.toString())
+    }
   }
 }
+*/
